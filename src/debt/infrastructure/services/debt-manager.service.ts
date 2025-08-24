64ea44cc6b70fb0833a5d032ad2ae62712a4bea4
Khang Nguyễn <thinhkhang97@gmail.com>
2025-08-24 15:35:31 +0700

    fix: refine monthly payment calculation in DebtManagerService
    - Updated the `getMonthlyPayment` method in `DebtManagerServiceImpl` to handle cases where the interest rate is zero, ensuring accurate calculations for monthly payments.
    - Enhanced logic to return the correct payment amount based on the current paid amount and due date, improving the service's reliability.
    
    These changes enhance the accuracy of debt payment calculations, providing users with more precise financial information.

--- src/debt/infrastructure/services/debt-manager.service.ts
+++ src/debt/infrastructure/services/debt-manager.service.ts
@@ -21,9 +21,15 @@ export class DebtManagerServiceImpl implements DebtManagerService {
     const today = new Date();
     const timeDiff = new Date(dueDate).getTime() - today.getTime();
     const timeDiffInMonths = Math.ceil(timeDiff / (1000 * 60 * 60 * 24 * 30));
+
+    if (rate === 0) {
+      return timeDiffInMonths === 0
+        ? amount - currentPaidAmount
+        : (amount - currentPaidAmount) / timeDiffInMonths;
+    }
+
     return (
-      ((amount - currentPaidAmount) * rate) /
-      100 /
+      ((amount - currentPaidAmount) * (rate / 100)) /
       (1 - Math.pow(1 + rate / 100, -timeDiffInMonths))
     );
   }
